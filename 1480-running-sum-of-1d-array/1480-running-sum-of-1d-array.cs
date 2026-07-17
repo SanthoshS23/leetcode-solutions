9eada6651b24a8e9dee8c18eba919ba0baff92fe
Santhosh S <157519763+SanthoshS23@users.noreply.github.com>
2026-07-17 09:48:07 +0530

    Time: 0 ms (100%), Space: 46.9 MB (87.37%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.cs
@@ -1,11 +1,9 @@
 public class Solution {
     public int[] RunningSum(int[] nums) {
         int[] NewArr = new int[nums.Length];
-        int count = nums[0];
         NewArr[0] = nums[0];
         for(int i=1;i<nums.Length;i++){
-            NewArr[i] = count + nums[i];
-            count = count + nums[i];
+            NewArr[i] = NewArr[i-1] + nums[i];
         }
         return NewArr;
     }
